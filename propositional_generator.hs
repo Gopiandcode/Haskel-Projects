995265e01fff9178293a759efdf63e091fd2cbc2
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-12-19 20:32:44 +0000

    finished up io for system

--- propositional_generator.hs
+++ propositional_generator.hs
@@ -1,3 +1,6 @@
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
+
 data Variable = P | Q | R 
 data Formula = Literal Variable | Not Formula | And Formula Formula | Or Formula Formula | Implication Formula Formula
 
@@ -25,6 +28,66 @@ randProposition gen = case ((fst res) `mod` 3) of
                 2  -> (R, snd res)
                 where 
                       res = next gen
+randTrueFormula :: RndGen -> Valuation -> (Formula, RndGen)
+randTrueFormula gen valuation = case evaluate valuation proposition of
+                                                True  -> (Literal proposition, finalGen)
+                                                False -> (Not (Literal proposition), finalGen)
+                                        where propositionRes = randProposition gen
+                                              proposition = fst propositionRes
+                                              finalGen = snd propositionRes
+
+
+
+randTruePropositions :: Int -> RndGen -> Valuation -> ([Formula],RndGen)
+randTruePropositions 0 gen _ = ([], gen)
+randTruePropositions n gen valuation = (formula : remain, finalGen)
+                                where propositionRes = randProposition gen
+                                      proposition = fst propositionRes
+                                      genA = snd propositionRes
+                                      remainRes = randTruePropositions (n-1) genA valuation
+                                      remain = fst remainRes
+                                      finalGen = snd remainRes
+                                      formula = case evaluate valuation proposition of
+                                                True  -> Literal proposition
+                                                False -> Not (Literal proposition)
+
+randFormulas :: Int -> Int -> RndGen -> Valuation -> Formula -> ([Formula], RndGen)
+randFormulas 0 _ gen _ _ = ([], gen)
+randFormulas n branching gen valuation formula = (expandedFormula : remainingFormulas, finalGen)
+                                        where expandedFormulaRes = growBranch branching valuation (formula, gen)
+                                              expandedFormula    = fst expandedFormulaRes
+                                              genA               = snd expandedFormulaRes
+                                              newvaluationRes    = randValuation genA
+                                              newvaluation       = fst newvaluationRes
+                                              genB               = snd newvaluationRes
+                                              newformulaRes      = randTrueFormula genB newvaluation
+                                              newformula         = fst newformulaRes
+                                              genC               = snd newformulaRes
+                                              remainingFormulaRes= randFormulas (n-1) branching genC newvaluation newformula
+                                              remainingFormulas  = fst remainingFormulaRes
+                                              finalGen           = snd remainingFormulaRes
+
+genFormulas :: Int -> Int -> RndGen -> ([Formula], RndGen)
+genFormulas n branching gen = randFormulas n branching finalGen newvaluation newformula
+                        where newvaluationRes = randValuation gen
+                              newvaluation    = fst newvaluationRes
+                              genA            = snd newvaluationRes
+                              newformulaRes   = randTrueFormula genA newvaluation
+                              newformula      = fst newformulaRes
+                              finalGen        = snd newformulaRes
+
+randValuation :: RndGen -> (Valuation, RndGen)
+randValuation gen = (Valuation resbA resbB resbC, finalGen)
+                where resA = randBool gen
+                      resbA = fst resA
+                      genA = snd resA
+                      resB = randBool genA
+                      resbB = fst resB
+                      genB = snd resB
+                      resC = randBool genB
+                      resbC = fst resC
+                      finalGen = snd resC
+                      
 
 randChoice :: RndGen -> [a] -> (a, RndGen)
 randChoice gen xs = (xs !! fromIntegral ((fst res) `mod` toInteger (length xs)), snd res)
@@ -144,12 +207,62 @@ grow valuation (formula, gen) = case formula of
                                         (newa, newgen) = grow valuation (a, gen)
                                         (newb, finalgen) = grow valuation (b, newgen)
 
+prettify :: [Formula] -> String
+prettify [] = ""
+prettify (x:xs) = (show x) ++ "\n" ++ (prettify xs)
 
-
--- growBranch :: Int -> Valuation -> (RndGen, Formula) -> (RndGen, Formula)
 growBranch 0 _ res = res
 growBranch n valuation res = growBranch (n-1) valuation (grow valuation res) 
 
 result = grow (Valuation True True True) ((Literal P), (RndGen 1))
+
+interactWith function inputFile outputFile = do
+        input <- readFile inputFile
+        writeFile outputFile (function input)
+
+printIntro = do
+            putStrLn "GopiandCode's Satisfiable Propositional Logic Formula Generator"
+            putStrLn "This simple tool will generate a set of satisfiable propositional logic formulae."
+            putStrLn "You can customize the complexity of the constructed formula by increasing the branching amount."
+getCount = do
+            putStrLn "How many formula would you like to generate?"
+            result <- getLine
+            case (readMaybe result) :: Maybe Int  of
+                        (Just x) -> do
+                                       return x
+                        Nothing  -> do
+                                        putStrLn "Sorry - that wasn't a valid integral value. Please try again."
+                                        getCount
+getComplexity = do
+            putStrLn "How complex should the formulae be? (3 is about right, though feel free to experiment)"
+            result <- getLine
+            case (readMaybe result) :: Maybe Int  of
+                        (Just x) -> do
+                                        return x
+                        Nothing  -> do
+                                        putStrLn "Sorry - that wasn't a valid integral value. Please try again."
+                                        getComplexity
+
+getOutputFile = do
+            putStrLn "Where should I write the file to? (if it exists I'll overwrite it)"
+            getLine
+
+main = do
+      args <- getArgs
+      case args of
+          [count,complexity,output]   ->  writeFile output $ prettify $ fst (genFormulas countValue complexityValue (RndGen 3))
+                                                        where countValue      = read (count) :: Int 
+                                                              complexityValue = read (complexity) :: Int
+          [count,complexity,output, seed]   ->  writeFile output $ prettify $ fst (genFormulas countValue complexityValue (RndGen $ toInteger seedValue))
+                                                        where countValue      = read (count) :: Int 
+                                                              complexityValue = read (complexity) :: Int
+                                                              seedValue       = read (seed) :: Int
+          _                -> do
+                                 printIntro
+                                 countValue <- getCount
+                                 complexityValue <- getComplexity
+                                 output <- getOutputFile
+                                 writeFile output $ prettify $ fst (genFormulas countValue complexityValue (RndGen 3))
+                                
 
 
